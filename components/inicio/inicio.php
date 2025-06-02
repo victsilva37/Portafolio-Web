db15cceb2f722a02cce722dfb8d2a5548dd50aa1
victsilva37 <victormau.sm@gmail.com>
2025-06-01 20:50:05 -0400

    Se  termina el inicio

---
+++ components/inicio/inicio.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <!--Link archivo CSS-->
+    <link rel="stylesheet" href="components/inicio/inicio.css">
+</head>
+<body>
+
+    <div id="main-banner-container">
+
+        <!--FOTO PERFIL-->
+
+            <img id="logo_perfil" src="assets/inicio/img/img1_foto_perfil.jpg" alt="">
+
+            <br>
+            
+
+        <!--DESCRIPCIÓN-->
+
+            <div id="IAM-content">
+                <h1>Víctor Silva Muñoz</h1>
+                <span>Ingeniero en Informática</span>
+            </div>
+            
+
+    </div>
+    
+</body>
+</html>
