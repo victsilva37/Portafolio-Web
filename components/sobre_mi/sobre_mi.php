6a5108477fb6e8c0e213e152871f0cc930fcacd7
victsilva37 <victormau.sm@gmail.com>
2025-06-01 20:56:43 -0400

    Sección sobre mí listo

---
+++ components/sobre_mi/sobre_mi.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Link archivo CSS-->
+    <link rel="stylesheet" href="components/sobre_mi/sobre_mi.css">
+
+</head>
+
+<body>
+    <div id="main-desc-container">
+
+        <h1>Sobre mí</h1>
+
+        <p>
+            Recientemente egresado de Ingeniería en Informática con mención en Ciencia de Datos, con sólidos
+            conocimientos en desarrollo de software y automatización de procesos. Destaco por mi capacidad para
+            adaptarme rápidamente a nuevos entornos y tecnologías, junto con una pasión por resolver problemas
+            complejos y contribuir a proyectos innovadores. Estoy en búsqueda de mi primer desafío profesional
+            como Desarrollador Frontend, Backend o Full Stack, donde pueda aplicar mis conocimientos en
+            entornos reales y generar impacto en las organizaciones.
+        </p>
+
+    </div>
+    
+</body>
+</html>
